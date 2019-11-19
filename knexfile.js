b2c5e6367c647bdd2cb5dc7793617bacea04011f
XAmi101 <famousvacancy@gmail.com>
2019-11-19 17:27:22 -0500

    edit to push to heroku.

--- knexfile.js
+++ knexfile.js
@@ -36,19 +36,22 @@ module.exports = {
   },
 
   production: {
-    client: 'postgresql',
+    client: 'sqlite3',
     connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
+      filename: './database/users.db3'
+    },
+    useNullAsDefault: true,
+    migrations: {
+      directory: "./database/migrations"
+    },
+    seeds: {
+      directory: "./database/seeds"
     },
     pool: {
-      min: 2,
-      max: 10
+      afterCreate: (conn, done) => {
+        conn.run('PRAGMA foreign_keys = ON', done);
+      },
     },
-    migrations: {
-      tableName: 'knex_migrations'
-    }
   }
 
 };
