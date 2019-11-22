6b6fb0d3443a920f31a22fb89eb4d749d74a0a1a
XAmi101 <famousvacancy@gmail.com>
2019-11-22 16:39:06 -0500

    update after fixing issues with git

--- database/db-config.js
+++ database/db-config.js
@@ -1,13 +1,7 @@
-// const knex = require("knex");
+const knex = require("knex");
 
-// const knexfile = require("../knexfile");
+const knexfile = require("../knexfile");
 
-
-// module.exports = knex(knexfile[environment]);
-
-const knex = require('knex');
-const knexConfig = require('../knexfile.js');
 const environment = process.env.NODE_ENV || "development";
 
-
-module.exports = knex(knexConfig.development);
+module.exports = knex(knexfile[environment]);
